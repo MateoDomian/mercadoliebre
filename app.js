16cb51a315a3e2600d705eb40e4294d02720196f
MateoDomian <mateodomian@gmail.com>
2021-09-20 14:21:46 -0300

    update2

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const port = 3030
 app.use(express.static('public'));
 
 app.get('/', (req,res) => {
     res.sendFile(path.resolve('./views/home.html'));
 });
 
-app.listen(3030, () => console.log ('servidor en puerto 3030'))
+app.listen(process.env.PORT || port, () => console.log ('servidor en puerto 3030'))
